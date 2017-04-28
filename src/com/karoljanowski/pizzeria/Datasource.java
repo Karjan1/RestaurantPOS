4b7ffb268ee16ae155928a4a5551110d9ffd3553
Karol Janowski <karol.janowski22@gmail.com>
2017-04-28 04:35:42 +0200

    v.1.3

--- src/com/karoljanowski/pizzeria/Datasource.java
+++ src/com/karoljanowski/pizzeria/Datasource.java
@@ -16,24 +16,36 @@ public class Datasource {
     public static final String TABLE_STORAGES = "storages";
     public static final String COLUMN_STORAGE_ID = "_id";
     public static final String COLUMN_STORAGE_NAME = "name";
+    public static final int INDEX_STORAGE_ID = 1;
+    public static final int INDEX_STORAGE_NAME = 2;
 
     public static final String TABLE_REGISTERS = "registers";
     public static final String COLUMN_REGISTER_ID = "_id";
     public static final String COLUMN_REGISTER_NAME = "name";
+    public static final int INDEX_REGISTER_ID = 1;
+    public static final int INDEX_REGISTER_NAME = 2;
 
     public static final String TABLE_BASKETS = "baskets";
     public static final String COLUMN_BASKET_ID = "_id";
     public static final String COLUMN_BASKET_NAME = "name";
     public static final String COLUMN_BASKET_VALUE = "value";
+    public static final int INDEX_BASKET_ID = 1;
+    public static final int INDEX_BASKET_NAME = 2;
+    public static final int INDEX_BASKET_VALUE = 3;
 
     public static final String TABLE_MENUS = "menus";
     public static final String COLUMN_MENU_ID = "_id";
     public static final String COLUMN_MENU_NAME = "name";
+    public static final int INDEX_MENU_ID = 1;
+    public static final int INDEX_MENU_NAME = 2;
 
     public static final String TABLE_PIZZAS = "pizzas";
     public static final String COLUMN_PIZZA_ID = "_id";
     public static final String COLUMN_PIZZA_NAME = "name";
     public static final String COLUMN_PIZZA_VALUE = "value";
+    public static final int INDEX_PIZZA_ID = 1;
+    public static final int INDEX_PIZZA_NAME = 2;
+    public static final int INDEX_PIZZA_VALUE = 3;
 
     public static final String TABLE_INGREDIENTS = "ingredients";
     public static final String COLUMN_INGREDIENT_ID = "_id";
@@ -41,14 +53,23 @@ public class Datasource {
     public static final String COLUMN_INGREDIENT_PRICE = "price";
     public static final String COLUMN_INGREDIENT_BOUGHT = "bought";
     public static final String COLUMN_INGREDIENT_USED = "used";
+    public static final int INDEX_INGREDIENT_ID = 1;
+    public static final int INDEX_INGREDIENT_NAME = 2;
+    public static final int INDEX_INGREDIENT_PRICE = 3;
+    public static final int INDEX_INGREDIENT_BOUGHT = 4;
+    public static final int INDEX_INGREDIENT_USED = 5;
 
     public static final String TABLE_DRINKS = "drinks";
     public static final String COLUMN_DRINK_ID = "_id";
     public static final String COLUMN_DRINK_NAME = "name";
+    public static final int INDEX_DRINK_ID = 1;
+    public static final int INDEX_DRINK_NAME = 2;
 
     public static final String TABLE_PIZZATOPPINGS = "pizzaToppings";
     public static final String COLUMN_PIZZATOPPING_ID = "_id";
     public static final String COLUMN_PIZZATOPPING_NAME = "name";
+    public static final int INDEX_PIZZATOPPING_ID = 1;
+    public static final int INDEX_PIZZATOPPING_NAME = 2;
 
     private Connection conn;
 
@@ -80,13 +101,11 @@ public class Datasource {
 
     }
 
-    public boolean open() {
+    public void open() {
         try {
             conn = DriverManager.getConnection(CONNECTION_STRING);
-            return true;
         } catch (SQLException e) {
             System.out.println("Couldn't connect to database: " + e.getMessage());
-            return false;
         }
     }
 
